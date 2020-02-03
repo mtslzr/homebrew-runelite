3a1eaa71c197e7e3d03bcb1b7a52771d05b06a4f
Matthew Salazar <m@tthewsalazar.com>
2020-02-03 15:27:24 -0600

    Updated source and macOS version to 2.1.1

--- Casks/runelite.rb
+++ Casks/runelite.rb
@@ -1,8 +1,8 @@
 cask 'runelite' do
-  version '1.6.5'
+  version '2.1.1'
   sha256 '6679be2a7c796ea3215e880a0c57c9a39010d89b1a03e147311489f1729b3e29'
 
-  url "https://github.com/runelite/runelite/archive/runelite-parent-#{version}.zip"
+  url "https://github.com/runelite/launcher/releases/download/#{version}/RuneLite.dmg"
   appcast 'https://github.com/runelite/runelite/releases.atom'
   name 'RuneLite'
   homepage 'https://runelite.net'

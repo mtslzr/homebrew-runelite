ef7920e2541e7feae5c3bb5bd4a1c885b89dc0f5
Matthew Salazar <m@tthewsalazar.com>
2020-08-01 18:47:24 +0000

    Update to 2.1.4

--- Casks/runelite.rb
+++ Casks/runelite.rb
@@ -1,6 +1,6 @@
 cask 'runelite' do
-  version '2.1.2'
-  sha256 '1a46793fcaeaa6528dfd894794b47276833b852926a33a8450c4161a5f723e13'
+  version '2.1.4'
+  sha256 '43b2abc09745caa77f389f9b924d01139fbd53818405959c3ad0586d84adb050'
 
   url "https://github.com/runelite/launcher/releases/download/#{version}/RuneLite.dmg"
   appcast 'https://github.com/runelite/launcher/releases.atom'
